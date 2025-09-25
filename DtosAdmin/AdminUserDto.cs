6a73c6a5863cc48f5b08f76f219e84b142af4843
AnaRadmanovic22 <anaradmanovic21@gmail.com>
2025-09-25 18:14:32 +0200

    Admin: GET i POST za upravljanje korisnicima

---
+++ DtosAdmin/AdminUserDto.cs
@@ -0,0 +1,12 @@
+﻿using gozba_na_klik.Enums;
+
+namespace gozba_na_klik.DtosAdmin
+{
+    public class AdminUserDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public Role Role { get; set; }
+    }
+}
